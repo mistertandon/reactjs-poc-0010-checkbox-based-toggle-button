9c52f29b90041dd7e96625feb41655fe0205a4b2
mistertandon <enggparveshtandon@gmail.com>
2023-03-27 18:13:01 +0530

    feat: Implement CSS style to make toggle button based on checkbox

--- src/components/RadioButton/RadioButton.jsx
+++ src/components/RadioButton/RadioButton.jsx
@@ -1,7 +1,39 @@
+import { useEffect, useRef } from "react";
 import "./RadioButton.scss";
 
 const RadioButton = () => {
-  return <>Hello from Radio Button</>;
+  const checkboxCheckedStatus = useRef(false);
+
+  const handleCheckboxChange = () => {
+    console.log("checkboxCheckedStatus", checkboxCheckedStatus.current.checked);
+    let ballContainerEle = document.querySelector(".ball--container");
+    if (checkboxCheckedStatus.current.checked) {
+      ballContainerEle.setAttribute("style", "transform:translateX(100%);");
+    }
+    if (!checkboxCheckedStatus.current.checked) {
+      ballContainerEle.setAttribute("style", "transform:translateX(0%);");
+    }
+  };
+
+  return (
+    <section className="radio-button--container">
+      <label htmlFor="toggle-checkbox">
+        <div className="box--container">
+          <div
+            id="rb-ball--container"
+            className="ball--container"
+          ></div>
+          <input
+            type="checkbox"
+            name="toggle-checkbox"
+            className="toggle-checkbox"
+            ref={checkboxCheckedStatus}
+            onChange={handleCheckboxChange}
+          />
+        </div>
+      </label>
+    </section>
+  );
 };
 
 export default RadioButton;
